1f2e118f54278ac0563e77ded96de3243912fc8d
M-Oirschot <marcovanoirschot@outlook.com>
2015-09-16 09:03:55 +0200

    Set up asterix solution

---
+++ Asterix/Asterix/Asterix.py
@@ -0,0 +1,13 @@
+﻿print ("Specify a number please.")
+end = 0
+try:
+   number = int(input())
+   numberStart = number
+except ValueError:
+   print("That's not an int!")
+else:
+    while number != -1:
+        print ("*" + "*" * number + " " * end + "*")
+        end = end + 1
+        number = number -1
+    print("*" * numberStart +"**")
