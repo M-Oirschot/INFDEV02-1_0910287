92f22bed2220ab11d177b683aadfcc825ea6fbec
M-Oirschot <marcovanoirschot@outlook.com>
2015-09-23 10:44:21 +0200

    1B

--- CelciusToKelvin/CelciusToKelvin/CelciusToKelvin.py
+++ CelciusToKelvin/CelciusToKelvin/CelciusToKelvin.py
@@ -1,8 +1,10 @@
-print("Input celcius")
-celcius = int(input(""))
+while True:
+    print("Input celcius")
+    celcius = int(input(""))
 
-if(celcius < -273):
-    print ("Value cannot be lower then -273")
-else:
-    kelvin = round(celcius + 273.15,0)
-    print (kelvin)
+    if(celcius < -273):
+        print ("Value cannot be lower then -273")
+    else:
+        kelvin = round(celcius + 273.15,0)
+        print (kelvin)
+        break
