319c6951761d2d844cee8735535bcf7c6afed0d3
M-Oirschot <marcovanoirschot@outlook.com>
2015-09-30 13:20:19 +0200

    python2.7

--- ReverseString/ReverseString/ReverseString.py
+++ ReverseString/ReverseString/ReverseString.py
@@ -1,4 +1,4 @@
-print("Waiting on user input")
-string = input("")
+﻿print("Waiting on user input")
+string = raw_input("")
 output = string[::-1] #docs.python.org/2/whatsnew/2.3.html#extended-slices
 print(output)
