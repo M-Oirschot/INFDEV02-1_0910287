7cfcb9101d0a7f7e1c0defab2f3f23eff153168a
Marco van Oirschot <marcovanoirschot@hotmail.com>
2015-10-06 17:20:00 +0200

    added loops as this was a mandatory that was added later...

--- ReverseString/ReverseString/ReverseString.py
+++ ReverseString/ReverseString/ReverseString.py
@@ -1,4 +1,20 @@
 ﻿print("Waiting on user input")
 string = raw_input("")
-output = string[::-1] #docs.python.org/2/whatsnew/2.3.html#extended-slices
+
+#method 1
+#output = string[::-1] #extended slices
+#saw it was a mandatory to use a loop @ github (pretty late to update though)
+
+#method 2
+getLength = len(string)
+char = getLength -1
+
+i = 0
+output = ""
+
+while(i != getLength):
+    output = output + string[char]
+    char -= 1
+    i += 1
+
 print(output)
